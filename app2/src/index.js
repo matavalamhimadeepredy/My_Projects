0e64f5f96634b9093b8b55192052e2f42d5a073c
himadeep <matavalamhimadeepreddy@gmail.com>
2024-06-14 11:29:22 +0530

    S

--- app2/src/index.js
+++ app2/src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import ReactDOM from 'react-dom/client';
 import './index.css';
-import './Gopi';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Gopi } from './Hooks/Gopi';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <Gopi/>
+    <App />
   </React.StrictMode>
-;
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
