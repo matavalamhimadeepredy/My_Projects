1192733396ef3182da4e1a2944c941e4f1a79879
himadeep <matavalamhimadeepreddy@gmail.com>
2024-06-15 07:30:32 +0530

    added

--- app2/src/App.js
+++ app2/src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import Users from "./Components/Users";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+function App() {                                                                                                    
+    return (
+      <div>
+       {/* <h3>welcome to himadeepreddy</h3> */}
+       {/* <Reddy/> */}
+       <Users/>
+                   </div>
+    );
+  }
+  
+  export default App;
