0bf124bfe5c85e5f1f24966e8a6cac71322d02d8
himadeep <matavalamhimadeepreddy@gmail.com>
2024-06-22 12:31:42 +0530

    add

---
+++ app3-intro/Local_server/boostrap/App.js
@@ -0,0 +1,13 @@
+import Sai from './Bootstrape-component/Sai'
+import VijayDinesh from './Bootstrape-component/VijayDinesh'
+
+function App () {
+  return (
+    <div>
+      <Sai />
+      <VijayDinesh />
+    </div>
+  )
+}
+
+export default App
